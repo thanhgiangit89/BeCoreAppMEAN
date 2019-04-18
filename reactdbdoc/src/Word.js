ad580420e4741856b37bf333d47e6d9cd5a555d6
TraBE <thanhgiangit89@gmail.com>
2019-04-18 09:10:38 +0700

    a

--- reactdbdoc/src/Word.js
+++ reactdbdoc/src/Word.js
@@ -1,12 +1,22 @@
 import React from 'react';
 //stateless component
 //functional component
-export const Word = (props) => (
-    <div className="container">
-        <h3 style={{ color: props.wordInfo.isMemorized ? 'green' : 'red' }}>
-            {props.wordInfo.en}
-        </h3>
-        <p>{props.wordInfo.vn}</p>
+export const Word = ({wordInfo}) => (
+    <div className="word" key={wordInfo._id}>
+        <div className="word-container">
+            <h3 className="text-success">{wordInfo.en}</h3>
+            <h3 className="text-danger">
+                {wordInfo.isMemorized ? '----' : wordInfo.vn}
+            </h3>
+        </div>
+        <div className="btn-container">
+            <button className="btn btn-success" >
+                {wordInfo.isMemorized ? 'Forgot' : 'Memorized'}
+            </button>
+            <button className="btn btn-warning" >
+                Remove
+          </button>
+        </div>
     </div>
 )
 
